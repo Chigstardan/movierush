3fd2f02507c27273bd3331e795c3d3a6abc44f02
Chigstardan <91606695+Chigstardan@users.noreply.github.com>
2021-10-07 08:50:39 +0100

    Update recent_release.py

--- recent_release.py
+++ recent_release.py
@@ -6,22 +6,22 @@ from urllib.request import Request, urlopen
 pages = ["movies", "series"]
 printed = []
 for page in pages:
-	req = Request("https://www.thenetnaija.com/videos/" + page, headers={'User-Agent': 'XYZ/3.0'})
+    req = Request("https://www.thenetnaija.com/videos/" + page, headers={'User-Agent': 'XYZ/3.0'})
 
-	webpage = urlopen(req, timeout=10)
+    webpage = urlopen(req, timeout=10)
 
-	b4 = BeautifulSoup(webpage, "html.parser")
+    b4 = BeautifulSoup(webpage, "html.parser")
 
-	movie_list = b4.find_all("div", {"class" : "video-files"})
+    movie_list = b4.find_all("div", {"class" : "video-files"})
 
 
-	for allContainers in movie_list:
-		filmName = allContainers.find('img').get('alt')
-		printed.append(filmName)
-		print(printed)
+    for allContainers in movie_list:
+        filmName = allContainers.find('img').get('alt')
+	printed.append(filmName)
+	print(printed)
 
 while True:
-	for get in printed:
-		if get not in printed:	
-			requests.get("https://api.telegram.org/bot2008959598:AAEapVykIXdphGYaH5ZjXuhpFaFw7wpi5Bs/sendMessage?chat_id=805427106&text={}".format(get))
-	time.sleep(3600)
+    for get in printed:
+	if get not in printed:	
+	    requests.get("https://api.telegram.org/bot2008959598:AAEapVykIXdphGYaH5ZjXuhpFaFw7wpi5Bs/sendMessage?chat_id=805427106&text={}".format(get))
+    time.sleep(3600)
